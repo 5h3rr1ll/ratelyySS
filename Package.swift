d4a383f7a0d43f7bf7a15d5d8b79325fefae270f
Akarsh Seggemu <sakarshkumar@gmail.com>
2018-10-26 18:43:04 +0200

    - Removed MongoDriver and FluentSqlite - Added Mongokitten

--- Package.swift
+++ Package.swift
@@ -6,15 +6,11 @@ let package = Package(
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-
-        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
-        
-        // Swift Community MongoDB Driver
-        .package(url: "https://github.com/vapor-community/mongo-driver", from: "1.0.0")
+        // Swift Community Mongokitten
+        .package(url: "https://github.com/OpenKitten/MongoKitten.git", from: "4.0.0")
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "MongoDriver"]),
+        .target(name: "App", dependencies: ["Vapor", "MongoKitten"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
